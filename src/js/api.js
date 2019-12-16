4ac97fb98ca3be06a01dc16e3f7b858c476c3d33
Anton Voitenko <voytenko.ad@yandex.ru>
2019-12-16 21:02:52 +0800

    Fixed card deletion, formatting code, deploy script

--- src/js/api.js
+++ src/js/api.js
@@ -1,91 +1,89 @@
-export class Api {
-    constructor(options) {
-        this.options = options;
-        this.getInitialCards = this.getInitialCards.bind(this);
-    }    
+export default class Api {
+  constructor(options) {
+    this.options = options;
+    this.getInitialCards = this.getInitialCards.bind(this);
+  }
 
-    getResponse(res) {
-        if(res.ok) {
-            return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так, ошибка: ${res.status}`);
+  getResponse(res) {
+    if (res.ok) {
+      return res.json();
     }
+    return Promise.reject(`Что-то пошло не так, ошибка: ${res.status}`);
+  }
 
-    async getInitialCards() {
-        return fetch(`${this.options.baseUrl}/cards`, {
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  async getInitialCards() {
+    return fetch(`${this.options.baseUrl}/cards`, {
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    getUserInfo() {
-        return fetch(`${this.options.baseUrl}/users/me`, {
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  getUserInfo() {
+    return fetch(`${this.options.baseUrl}/users/me`, {
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    changeUserInfo(name, job) {
-        return fetch(`${this.options.baseUrl}/users/me`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-                name: name,
-                about: job
-            }),
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  changeUserInfo(name, job) {
+    return fetch(`${this.options.baseUrl}/users/me`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        name,
+        about: job,
+      }),
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    addNewCard(name, link) {
-        return fetch(`${this.options.baseUrl}/cards`, {
-            method: 'POST',
-            body: JSON.stringify({
-                name: name,
-                link: link
-            }),
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  addNewCard(name, link) {
+    return fetch(`${this.options.baseUrl}/cards`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    deleteCard(id) {
-        return fetch(`${this.options.baseUrl}/cards/${id}`, {
-            method: 'DELETE',         
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  deleteCard(id) {
+    return fetch(`${this.options.baseUrl}/cards/${id}`, {
+      method: 'DELETE',
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    addLike(id) {
-        return fetch(`${this.options.baseUrl}/cards/like/${id}`, {
-            method: 'PUT',
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  addLike(id) {
+    return fetch(`${this.options.baseUrl}/cards/like/${id}`, {
+      method: 'PUT',
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    removeLike(id) {
-        return fetch(`${this.options.baseUrl}/cards/like/${id}`, {
-            method: 'DELETE',
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  removeLike(id) {
+    return fetch(`${this.options.baseUrl}/cards/like/${id}`, {
+      method: 'DELETE',
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 
-    changeAvatar(link) {
-        return fetch(`${this.options.baseUrl}/users/me/avatar`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-                avatar: link
-            }),
-            headers: this.options.headers
-        })
-            .then(this.getResponse);
-    }
+  changeAvatar(link) {
+    return fetch(`${this.options.baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        avatar: link,
+      }),
+      headers: this.options.headers,
+    })
+      .then(this.getResponse);
+  }
 }
-
-
 
 
 /*
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,19 @@
 import '../pages/index.css';
-import { Api } from './api';
-import { CardList } from './cardList.js';
+import Api from './api';
+import CardList from './cardList.js';
 import './popupsData';
-import { FullImgPopup, AddCardPopup, EditProfilePopup, AvatarPopup } from './popup.js';
-import { Validation } from './validation.js';
+import {
+  FullImgPopup, AddCardPopup, EditProfilePopup, AvatarPopup,
+} from './popup.js';
+import Validation from './validation.js';
 
 const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort5' : 'https://praktikum.tk/cohort5';
 const serverData = {
-    baseUrl: serverUrl,
-    headers: {
-        authorization: '0d37a330-306d-4e7c-a539-b36cb777efcc',
-        'Content-Type': 'application/json'
-    }
+  baseUrl: serverUrl,
+  headers: {
+    authorization: '0d37a330-306d-4e7c-a539-b36cb777efcc',
+    'Content-Type': 'application/json',
+  },
 };
 const popupBlock = document.querySelector('.popup');
 const placeList = document.querySelector('.places-list');
@@ -20,72 +22,72 @@ export const cardList = new CardList(placeList, cards);
 const api = new Api(serverData);
 
 function getUserInfo(result) {
-    const name = document.querySelector('.user-info__name');
-    const job = document.querySelector('.user-info__job');
-    const avatar = document.querySelector('.user-info__photo');
-    
-    name.textContent = result.name;
-    job.textContent = result.about;
-    avatar.style.backgroundImage = `url(${result.avatar})`;
+  const name = document.querySelector('.user-info__name');
+  const job = document.querySelector('.user-info__job');
+  const avatar = document.querySelector('.user-info__photo');
+
+  name.textContent = result.name;
+  job.textContent = result.about;
+  avatar.style.backgroundImage = `url(${result.avatar})`;
 }
 
 api.getInitialCards()
-    .then(result => {
-        cardList.getCardsFromServer(result);
-    })
-    .catch(err => console.log(`${err}. Возникла проблема с загрузкой карточек с сервера.`));
+  .then((result) => {
+    cardList.getCardsFromServer(result);
+  })
+  .catch((err) => console.log(`${err}. Возникла проблема с загрузкой карточек с сервера.`));
 
 api.getUserInfo()
-    .then(result => {
-        getUserInfo(result);
-    })
-    .catch(err => {
-        console.log(`${err}. Проблема в получении данных пользователя.`);
-    });
+  .then((result) => {
+    getUserInfo(result);
+  })
+  .catch((err) => {
+    console.log(`${err}. Проблема в получении данных пользователя.`);
+  });
 
-document.addEventListener('click', event => {
-    if (event.target.classList.contains('place-card__image')) {
-        const popup = new FullImgPopup(popupBlock, window.popupsData.fullImgPopup);
-        
-        popup.listeners();
-        popup.open();
-        popup.addLink(event.target.style.backgroundImage.slice(5, -2));
-    }
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('place-card__image')) {
+    const popup = new FullImgPopup(popupBlock, window.popupsData.fullImgPopup);
+
+    popup.listeners();
+    popup.open();
+    popup.addLink(event.target.style.backgroundImage.slice(5, -2));
+  }
 });
 
 document.querySelector('.user-info__button').addEventListener('click', () => {
-    const popup = new AddCardPopup(popupBlock, window.popupsData.addPopup);
-    
-    popup.open();
-    popup.listeners();
-    popup.hideListeners();
+  const popup = new AddCardPopup(popupBlock, window.popupsData.addPopup);
 
-    const valid = new Validation(document.querySelector('.popup__form'));
-    valid.render();
+  popup.open();
+  popup.listeners();
+  popup.hideListeners();
+
+  const valid = new Validation(document.querySelector('.popup__form'));
+  valid.render();
 });
 
 document.querySelector('.user-info__edit-button').addEventListener('click', () => {
-    const popupEdit = new EditProfilePopup(popupBlock, window.popupsData.editPopup);
+  const popupEdit = new EditProfilePopup(popupBlock, window.popupsData.editPopup);
 
-    popupEdit.open();
-    popupEdit.getCurrentProfile();
-    popupEdit.listeners();
-    popupEdit.hideListeners();
+  popupEdit.open();
+  popupEdit.getCurrentProfile();
+  popupEdit.listeners();
+  popupEdit.hideListeners();
 
-    const valid = new Validation(document.querySelector('.popup__form'));
-    valid.render();
+  const valid = new Validation(document.querySelector('.popup__form'));
+  valid.render();
 });
 
 document.querySelector('.user-info__photo').addEventListener('click', (event) => {
-    event.preventDefault();
-    const popup = new AvatarPopup(popupBlock, window.popupsData.avatar);
+  event.preventDefault();
+  const popup = new AvatarPopup(popupBlock, window.popupsData.avatar);
 
-    popup.open();
-    popup.listeners();
-    popup.hideListeners();
+  popup.open();
+  popup.listeners();
+  popup.hideListeners();
 
-    const valid = new Validation(document.querySelector('.popup__form'));
-    valid.render();
+  const valid = new Validation(document.querySelector('.popup__form'));
+  valid.render();
 });
 
-export {api};
+export { api };
