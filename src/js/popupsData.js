b6bce915233edb332f4afe5fada0055996c5216d
Anton Voitenko <voytenko.ad@yandex.ru>
2019-12-16 22:50:34 +0800

    Fixed card deletion, formatting code, deploy script

--- src/js/popupsData.js
+++ src/js/popupsData.js
@@ -1,9 +1,10 @@
 import closeImage from '../images/close.svg';
-(function() {
-        const popupsData = {
+
+(function () {
+  const popupsData = {
     addPopup: `
         <div class="popup__content">
-        <img src="${ closeImage }" alt="" class="popup__close">
+        <img src="${closeImage}" alt="" class="popup__close">
         <h3 class="popup__title">Новое место</h3>
         <form class="popup__form" name="new" novalidate>
             <input class="popup__input popup__input-type-name" placeholder="Название" name="name" required minlength="2" maxlength="30">
@@ -15,7 +16,7 @@ import closeImage from '../images/close.svg';
         </div>`,
     editPopup: `
         <div class="popup__content">
-            <img src="${ closeImage }" alt="" class="popup__close">
+            <img src="${closeImage}" alt="" class="popup__close">
             <h3 class="popup__title">Редактировать профиль</h3>
             <form class="popup__form" name="editProfile" novalidate>
                 <input type="text" name="userName" class="popup__input popup__input-type-user-name" placeholder="Имя" required minlength="2" maxlength="30">
@@ -27,21 +28,20 @@ import closeImage from '../images/close.svg';
         </div>`,
     fullImgPopup: `
         <div class="popup__form full-image-popup__content">
-            <img src="${ closeImage }" alt="" class="popup__close">
+            <img src="${closeImage}" alt="" class="popup__close">
             <img src="" alt="" class="full-image-popup__pic">
         </div>`,
     avatar: `
         <div class="popup__content">
-            <img src="${ closeImage }" alt="" class="popup__close">
+            <img src="${closeImage}" alt="" class="popup__close">
             <h3 class="popup__title">Изменить автар</h3>
             <form class="popup__form" name="EditAvatar" novalidate>
                 <input name="avatar" class="popup__input popup__input-type-avatar" placeholder="Ссылка на автатар" required type="url" required>
                 <span class="popup__error-message popup__error-message_first" aria-live="polite">&nbsp;</span>
                 <button type="submit" class="button popup__button popup__button_avatar popup__button_edit-profile" name='confirmChanges' disabled>Сохранить</button>
             </form>
-        </div>`
-}
+        </div>`,
+  };
 
-window.popupsData = popupsData;
-})();
-
+  window.popupsData = popupsData;
+}());
